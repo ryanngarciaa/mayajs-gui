93828cc4e76c55304ec600fd5cc8a3dcf9861ed4
Mack Racket <mack_ignacio@yahoo.com>
2021-03-19 10:28:21 +0800

    Add create project button

--- src/views/CreateProject.vue
+++ src/views/CreateProject.vue
@@ -1,5 +1,10 @@
 <template>
   <div>Create Project Page</div>
+  <div class="position-absolute bottom-0 start-50 translate-middle-x p-5">
+    <button type="button" class="btn btn-danger">
+      Create a new project here
+    </button>
+  </div>
 </template>
 
 <script>

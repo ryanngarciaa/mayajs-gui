1ca38d12a486a54cf7847bee387cd9e023f3efed
Mack Racket <mack_ignacio@yahoo.com>
2021-03-19 17:56:27 +0800

    Add preset component

--- src/components/Presets.vue
+++ src/components/Presets.vue
@@ -1,5 +1,19 @@
 <template>
   <div>presets</div>
+  <div class="row justify-content-center p-3">
+    <div class="col-8 col-md-4 p-3">
+      <div class="alert alert-primary" role="alert">
+        A preset is an association of plugins and configurations. After you've
+        selected features, you can optionally save it as a preset so that you
+        can reuse it for future projects, without having to reconfigure
+        everything again.
+      </div>
+    </div>
+    <div class="w-100 d-none d-md-block p-1"></div>
+    <div class="col-6 col-sm-4 p-3 border bg-light">
+      <h6 class="p-1">Select a preset</h6>
+    </div>
+  </div>
 </template>
 
 <script>
